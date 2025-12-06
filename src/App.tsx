0873fc74071f87d9b6474aec779fcc3f4497d8d4
Ansu Sharma <ansusharma@Ansus-MacBook-Air.local>
2025-12-06 22:07:10 +0530

    fix: remove absolute imports and add vite alias

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "/Users/ansusharma/codes/src/components/pages/Index.tsx";
-import NotFound from "/Users/ansusharma/codes/src/components/pages/NotFound";
+import Index from "@/components/pages/Index";
+import NotFound from "@/components/pages/NotFound";
 
 const queryClient = new QueryClient();
 
